fdabef723b88bc218adadcc0cee321b1a0fe8f17
koyuri98 <yuriiu4919@gmail.com>
2021-04-18 17:37:49 +0900

    테스트모달 생성 -> 모달 생성시 참고하면됨

--- src/components/modals/test.js
+++ src/components/modals/test.js
@@ -4,15 +4,15 @@ import { Modal, ModalHeader, ModalFooter, ModalBody } from '../Modal';
 const Test = props => {  
   return(
     <Modal className="Alert-box">
-        <ModalHeader>
-            <p>모달헤더임다</p>
-        </ModalHeader>
-        <ModalBody>
-            <p>모달몸텅임다</p>
-        </ModalBody>
-        <ModalFooter>
-            <p>모달푸터임다</p>
-        </ModalFooter>
+      <ModalHeader>
+          <p>모달헤더임다</p>
+      </ModalHeader>
+      <ModalBody>
+          <p>모달몸텅임다</p>
+      </ModalBody>
+      <ModalFooter>
+          <p>모달푸터임다</p>
+      </ModalFooter>
     </Modal>
   )
 }
