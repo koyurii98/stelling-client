a7ceebf2962b1cd2080ac41de7f4d45b8e282bed
jojunmyeong <a8456452@naver.com>
2021-04-19 10:52:51 +0900

    sidepage menu update.

--- src/components/layout/SidePageMenu.js
+++ src/components/layout/SidePageMenu.js
@@ -8,11 +8,11 @@ const SidePageMenu = (props) => {
   return(
      <div className={test}>
       <div className={menuTit}>
-        <span>{item.name}</span>
+        <span>{item.title}</span>
       </div>
       <List component="nav" aria-label="secondary mailbox folder" style={{padding:0}}>
         {
-          pageList && pageList.map((menuItem,i)=>{
+          pageList ? pageList.map((menuItem,i)=>{
             return <ListItem
                 key={i}
                 button
@@ -26,6 +26,8 @@ const SidePageMenu = (props) => {
               </div>
             </ListItem>
           })
+          :
+          <ListItem style={{ fontSize: "0.76vw" }}>아직 페이지가 없습니다. 페이지를 추가해보세요.</ListItem>
         }
       </List>
     </div>
