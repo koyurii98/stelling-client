3ab94938e280e4a6a16d623ca24b240d0d50031d
kimminha <alsgk9298@gmail.com>
2021-04-27 22:26:38 +0900

    글작성화면 작업

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import '../css/Write.css'
 
 const Write = () => {
   return(
-    <div>
-      <p>WRITE</p>
+    <div id="wrap">
+      <div id="content">
+        <div className="title_btns">
+          <div className="title">
+            <p className="write-tit">글 작성</p>
+            <div className="subject-name">국어</div>
+          </div>
+
+          <div className="btns">
+            <a href="" className="cancel">작성취소</a>
+            <a href="" className="write-ok">작성완료</a>
+          </div>
+        </div>
+
+        <input className="input-title" placeholder="제목을 입력해주세요."></input>
+        <input className="input-content" placeholder="내용을 입력해주세요."></input>
+      </div>
     </div>
   )
 }
