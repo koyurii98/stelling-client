52a36974a30c81ebb0585166c309d2463ac0a49b
koyuri <rmeodprp0856@naver.com>
2021-04-28 12:33:26 +0900

    전체 페이지 사이드메뉴 추가

--- src/components/layout/SidePageMenu.js
+++ src/components/layout/SidePageMenu.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
 import { List,ListItem } from '@material-ui/core';
+import { useHistory } from 'react-router';
 
 const SidePageMenu = (props) => {
   const { test, item, menuTit } = props;
   const pageList = item.pageList;
+  const history = useHistory();
+
+  const addPage = useCallback(() =>{
+    console.log("Add");
+    history.push('/write')
+  },[history]);
 
   return(
      <div className={test}>
       <div className={menuTit}>
         <span>{item.title}</span>
       </div>
-      <List component="nav" aria-label="secondary mailbox folder" style={{padding:0}}>
-        {
-           pageList && pageList.map((menuItem,i)=>{
-            return <ListItem
-                key={i}
-                button
-                // selected={selectedIndex === i}
-                // onClick={() => handleListItemClick(i, data)}
-                style={{height:"2.8vw"}}
-              >
-              <div className="SidePageMenu-text">
-                <span>{menuItem.title}</span>
-                <span className="SidePageMenu-text-date">{menuItem.date}</span>
-              </div>
-            </ListItem>
-          })
-        }
-      </List>
+      <div>
+        <List component="nav" aria-label="secondary mailbox folder" style={{padding:0}}>
+          {
+            pageList && pageList.map((menuItem,i)=>{
+              return <ListItem
+                  key={i}
+                  button
+                  style={{height:"2.8vw"}}
+                >
+                <div className="SidePageMenu-text">
+                  <span>{menuItem.title}</span>
+                  <span className="SidePageMenu-text-date">{menuItem.date}</span>
+                </div>
+              </ListItem>
+            })
+          }
+        </List>
+        {/* <div className="sideMenu-Btn" onClick={addPage}>
+          <span>페이지 추가</span>
+        </div> */}
+      </div>
     </div>
   )
 }
